cbda85b3f835ff2fcbfb627ef7ef2baedeb6b5bc
Trinity737 <85462888+Trinity737@users.noreply.github.com>
2021-11-21 18:41:56 -0500

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,23 @@
 import "./App.css";
+import Weather from "./Weather";
+import "./Weather.css";
 
 export default function App() {
   return (
-    <div className="container">
-      <header className="App-header">
-        <h1>Prompt Weather</h1>
-        <ul>
-          <li className="date">Wednesday 12:00pm</li>
-          <li className="description">Partly Cloudy</li>
-        </ul>
-      </header>
-      <img
-        src="https://ssl.gstatic.com/onebox/weather/64/rain.png"
-        alt="rain"
-        className="float-left"
-      />
-      <div className="float-left">
-        <div className="row">
-          <div className="col-6">24°</div>
-          <a href="">C</a> | <a href="">F</a>
-        </div>
-        <ul>
-          <li>Humidity: 80%</li>
-          <li>Wind: 5 km/h</li>
-        </ul>
+    <div className="App">
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by Cheavon Pottinger and is{" "}
+          <a
+            href="https://github.com/Trinity737/Prompt-Weather"
+            target="_blank"
+            alt="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
       </div>
-      <WeatherForecast />
-      <footer>
-        This project was coded by Cheavon Pottinger and is{" "}
-        <a
-          href="https://github.com/Trinity737/Prompt-Weather"
-          target="_blank"
-          alt="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <h1>Toronto</h1>
+      <ul>
+        <li className="date">Wednesday 12:00pm</li>
+        <li className="description">Partly Cloudy</li>
+      </ul>
+      <div className="float-left">
+        <div className="row">
+          <div className="col-6">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/rain.png"
+              alt="rain"
+              className="float-left"
+            />
+            24°
+            <a href="">C</a> | <a href="">F</a>
+          </div>
+        </div>
+      </div>
+      <div className="col-6">
+        <ul>
+          <li>Humidity: 80%</li>
+          <li>Wind: 5 km/h</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
