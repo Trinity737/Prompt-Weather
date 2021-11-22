c7f8d7bb109e5bf8de01a2dac7fef6f385ebc752
Trinity737 <85462888+Trinity737@users.noreply.github.com>
2021-11-22 01:26:04 -0500

    test

--- src/Weather.js
+++ src/Weather.js
@@ -6,6 +6,7 @@ import "./Weather.css";
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
   const [city, setCity] = useState(props.defaultCity);
+
   function handleResponse(response) {
     setWeatherData({
       ready: true,
@@ -13,17 +14,12 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
-      iconUrl: ` http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      icon: response.data.weather[0].icon,
       wind: response.data.wind.speed,
       city: response.data.name,
     });
   }
 
-  function search() {
-    const apiKey = "3e2ce69ce76e734e0f6e4dbc39c85c56";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-  }
   function handleSubmit(event) {
     event.preventDefault();
     search();
@@ -31,6 +27,12 @@ export default function Weather(props) {
 
   function handleCityChange(event) {
     setCity(event.target.value);
+  }
+
+  function search() {
+    const apiKey = "3e2ce69ce76e734e0f6e4dbc39c85c56";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   if (weatherData.ready) {
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,38 +1,39 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
-export default function WeatherInfo() {
+export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
-      {" "}
-      <h1>{weatherData.city}</h1>
+      <h1>{props.data.city}</h1>
       <ul>
         <li className="date">
-          <FormattedDate date={weatherData.date} />
+          <FormattedDate date={props.data.date} />
         </li>
-        <li className="text-capitalize">{weatherData.description}</li>
+        <li className="text-capitalize">{props.data.description}</li>
       </ul>
       <div className="float-left">
         <div className="row mt-3">
           <div className="col-6">
             <div className="clearfix">
-              <img
-                src={weatherData.iconUrl}
-                alt={weatherData.description}
-                className="float-left"
-              />
-              <span className="float-left">
+              <div className="float-left">
+                <WeatherIcon
+                  code={props.data.icon}
+                  alt={props.data.description}
+                />
+              </div>
+              <div className="float-left">
                 <span className="temperature">
-                  {Math.round(weatherData.temperature)}
+                  {Math.round(props.data.temperature)}
                 </span>
                 <span className="unit">℃</span>
-              </span>
+              </div>
             </div>
           </div>
           <div className="col-6">
             <ul>
-              <li>Humidity: {weatherData.humidity}%</li>
-              <li>Wind: {weatherData.wind}km/h</li>
+              <li>Humidity: {props.data.humidity}%</li>
+              <li>Wind: {props.data.wind}km/h</li>
             </ul>
           </div>
         </div>
